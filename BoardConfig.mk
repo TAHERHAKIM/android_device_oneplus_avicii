d333a5db52e8498f407d948dca577e29d9b4c5ed
Bruno Martins <bgcngm@gmail.com>
2025-07-06 16:54:45 -0400

    avicii: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,9 +66,6 @@ TARGET_SCREEN_DENSITY := 440
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
